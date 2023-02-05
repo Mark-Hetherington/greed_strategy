1b1715adb432816d0da20647d7fd1856d51b5b77
Mark Hetherington <mark@hethos.org>
2023-02-05 17:16:56 +1100

    Add another strategy

--- strategies/roll_multi.py
+++ strategies/roll_multi.py
@@ -38,3 +38,18 @@ class TakeScoresAndAlwaysRollAgainStrategy(PlayerStrategy):
             if die in [DieRoll.diamond, DieRoll.gold]:
                 selected_dice.append(die)
         return selected_dice, True
+
+class TakeScoresAndRollAtLeastThreeAgainStrategy(PlayerStrategy):
+    def select_dice_move(self, rolled_dice: List[DieRoll]) -> Tuple[List[DieRoll], bool]:
+        rolled = rolled_dice.copy()
+        selected_dice = []
+        for score in SCORE_PRIORITY:
+            if rolled_dice.count(score[0]) > score[1]:
+                selected_dice.extend([score[0]] * score[1])
+                for _ in range(score[1]):
+                    rolled.remove(score[0])
+        for die in rolled:            
+            if die in [DieRoll.diamond, DieRoll.gold]:
+                selected_dice.append(die)
+                rolled.remove(die)
+        return selected_dice, len(rolled) >= 3
