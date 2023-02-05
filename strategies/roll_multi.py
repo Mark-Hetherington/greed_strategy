9fcfd2b152650650c9eba2e2c48da3642361c170
Mark Hetherington <mark@hethos.org>
2023-02-05 17:41:55 +1100

    update multi roll strats

--- strategies/roll_multi.py
+++ strategies/roll_multi.py
@@ -6,6 +6,7 @@ from strategies.base import PlayerStrategy
 SCORE_PRIORITY = [
     (DieRoll.diamond, 4),
     (DieRoll.silver, 3),
+    (DieRoll.gold, 3),
     (DieRoll.ruby, 3),
     (DieRoll.ebony, 3),
     (DieRoll.emerald, 3)
@@ -13,14 +14,14 @@ SCORE_PRIORITY = [
 
 class TakeScoresAndDontRollAgainStrategy(PlayerStrategy):
     def select_dice_move(self, rolled_dice: List[DieRoll]) -> Tuple[List[DieRoll], bool]:
-        rolled = rolled_dice.copy()
+        remaining = rolled_dice.copy()
         selected_dice = []
         for score in SCORE_PRIORITY:
             if rolled_dice.count(score[0]) >= score[1]:
                 selected_dice.extend([score[0]] * score[1])
                 for _ in range(score[1]):
-                    rolled.remove(score[0])
-        for die in rolled:            
+                    remaining.remove(score[0])
+        for die in remaining:            
             if die in [DieRoll.diamond, DieRoll.gold]:
                 selected_dice.append(die)
         return selected_dice, False
@@ -34,22 +35,27 @@ class TakeScoresAndAlwaysRollAgainStrategy(PlayerStrategy):
                 selected_dice.extend([score[0]] * score[1])
                 for _ in range(score[1]):
                     rolled.remove(score[0])
-        for die in rolled:            
+        for die in rolled:
             if die in [DieRoll.diamond, DieRoll.gold]:
                 selected_dice.append(die)
         return selected_dice, True
 
-class TakeScoresAndRollAtLeastThreeAgainStrategy(PlayerStrategy):
+class TakeScoresAndRollAtLeastNAgainStrategy(PlayerStrategy):
+    def __init__(self, roll_threshold) -> None:
+        super().__init__()
+        self.roll_threshold = roll_threshold
+
+
     def select_dice_move(self, rolled_dice: List[DieRoll]) -> Tuple[List[DieRoll], bool]:
-        rolled = rolled_dice.copy()
+        remaining = rolled_dice.copy()
         selected_dice = []
         for score in SCORE_PRIORITY:
-            if rolled_dice.count(score[0]) > score[1]:
+            if rolled_dice.count(score[0]) >= score[1]:
                 selected_dice.extend([score[0]] * score[1])
                 for _ in range(score[1]):
-                    rolled.remove(score[0])
-        for die in rolled:            
-            if die in [DieRoll.diamond, DieRoll.gold]:
-                selected_dice.append(die)
-                rolled.remove(die)
-        return selected_dice, len(rolled) >= 3
+                    remaining.remove(score[0])
+        if len(remaining) <= self.roll_threshold:
+            for die in remaining:
+                if die in [DieRoll.diamond, DieRoll.gold]:
+                    selected_dice.append(die)
+        return selected_dice, len(remaining) >= self.roll_threshold
